3d8d6cc8ddb669a9a18d9d57e8440a7220da57e2
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-14 15:02:32 -0300

    Função log do console

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('Marcos Aleixo');
+console.log("Marcos Aleixo");
+console.log('Marcos "Aleixo"');
+console.log("Marcos 'Aleixo'");
+console.log(`Marcos Aleixo`);
+console.log(35, 15.85, 'Olá');
