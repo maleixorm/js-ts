d740ed5bc71600b8370bc0f8f01eadbe4934a40c
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-04 21:08:53 -0300

    Constantes no JS

---
+++ aula7/index.js
@@ -0,0 +1,13 @@
+const nome = 'João';
+console.log(nome);
+// + - * / %
+const primeiroNumero = '5';
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+console.log(resultadoTriplicado);
+console.log(primeiroNumero + segundoNumero);
+console.log(typeof(primeiroNumero + segundoNumero));
+console.log(typeof primeiroNumero);
